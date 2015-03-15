71f94e7fd86ca1775108e92eb43a3cd7aab942a3
Fernando Daciuk <f.daciuk@gmail.com>
2015-03-15 23:41:00 +0000

    Add gulpfile for tests

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var gulp = require( 'gulp' );
+var mocha = require( 'gulp-mocha' );
+var istanbul = require( 'gulp-istanbul' );
+
+gulp.task( 'mocha', function() {
+  gulp.src( './tests/*.js', { read : false })
+  .pipe(
+    mocha({ reporter: 'list' })
+  );
+});
+
+gulp.task( 'test', function( cb ) {
+  gulp.src( [ './tests/**/*.js', './lib/hoost.js' ])
+    .pipe( istanbul() )
+    .on( 'finish', function() {
+      gulp.src([ './tests/**/*.js', './lib/hoost.js' ])
+      .pipe( mocha() )
+      .pipe( istanbul.writeReports() )
+      .on( 'end', cb );
+    });
+});
+
+gulp.task( 'default', function() {
+  gulp.watch( [ './tests/**/*.js', './lib/hoost.js' ], [ 'test' ]);
+});
