ce8c43f5408fd4ea01a6a6d19cd5b46703b4f4d0
Fernando Daciuk <f.daciuk@gmail.com>
2015-08-27 13:42:20 -0300

    Add command update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@
 var gulp = require( 'gulp' );
 var mocha = require( 'gulp-mocha' );
 var istanbul = require( 'gulp-istanbul' );
+var exec = require( 'child_process' ).exec;
+var pkg = require( './package.json' );
 
 var testFiles = 'test/*.js';
 var coreFiles = 'lib/*.js';
@@ -19,6 +21,22 @@ gulp.task( 'test', function( cb ) {
         .pipe( istanbul.writeReports() )
         .on( 'end', cb );
     });
+});
+
+gulp.task( 'update-dependencies', function( cb ) {
+  var dependencies = Object.keys( pkg.dependencies );
+  var devDependencies = Object.keys( pkg.devDependencies );
+  var commandDependencies = 'npm i --save' + dependencies.reduce( reduce );
+  var commandDevDependencies = 'npm i --save-dev' + dependencies.reduce( reduce );
+
+  exec( commandDependencies + ' && ' + commandDevDependencies,
+  function(err, stdout, stdin) {
+    console.log( stdin );
+  });
+
+  function reduce(accumulated, item) {
+    return accumulated + ' ' + item;
+  }
 });
 
 gulp.task( 'default', [ 'test' ], function() {
