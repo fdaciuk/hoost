1a85f155f9772884295a5a6947107670043d0854
Fernando Daciuk <f.daciuk@gmail.com>
2015-09-02 07:26:04 -0300

    Improve test hoost-add

--- test/hoost-add.test.js
+++ test/hoost-add.test.js
@@ -18,8 +18,31 @@ describe( 'HOOST #add', function() {
 
   it( 'Should return error when don\'t pass params', function( done ) {
     exec( hoost + ' add', function( err, stdout, stdin ) {
-      console.log(stdin);
       stdin.should.match( /ERROR: Missing required argument "ip"/ );
+      done();
+    });
+  });
+
+  it( 'Should add IP/HOST: 127.0.0.1 test.com.br in /etc/hosts',
+  function( done ) {
+    exec( hoost + ' add 127.0.0.1 test.com.br',
+    function( err, stdout, stderr ) {
+      if( err ) throw err;
+      stdout.should.match( /127.0.0.1 test.com.br saved!/ );
+      done();
+    });
+  });
+
+  it( 'Before to add, should ask if want to really add an exist host',
+  function( done ) {
+    exec([
+      hoost,
+      ' add 127.0.0.1 test.com.br && ',
+      hoost,
+      ' add 127.0.0.1 test.com.br'
+    ].join( '' ), function( err, stdout, stderr ) {
+      if( err ) throw err;
+      stdout.should.match( /has been added already/ );
       done();
     });
   });
